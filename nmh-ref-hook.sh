8ab74c9cbd9e1191ea8483b004665ba1e445c80a
Kevin Layer <layer@known.net>
2013-03-15 06:06:46 -0700

    nmh-ref-hook.sh: efficiency hacks from Robert Elz
    Don't need to use sed a bunch of times.  For as long as I've used
    bash, I really need to learn those operators.

--- nmh-ref-hook.sh
+++ nmh-ref-hook.sh
@@ -3,24 +3,24 @@
 # Use the refile hook to make sure messages moved in the `important' sequence
 # retain membership in that sequence in their destination folder.
 
-prefix=$(mhpath +inbox | sed 's,/inbox$,,')
+prefix=$(mhpath +)
 
-to=$(echo $2 | sed -e "s|^$prefix/||")
+to=${2#${prefix}/}
 
 case $to in
     *trash*) ;;
-    *)  from=$(echo $1 | sed -e "s|^$prefix/||")
+    *)  from=${1#${prefix}/}
 
-	fromf=$(echo $from | sed -e 's,\(.*\)/\(.*\),\1,')
-	tof=$(echo $to | sed -e 's,\(.*\)/\(.*\),\1,')
+	fromf=${from%/*}
+	tof=${to%/*}
 
 	# In the case of a repack, sequences are handled properly, so exit
 	if [ "$fromf" = "$tof" ]; then
 	    exit 0
 	fi
 
-	fromm=$(echo $from | sed -e 's,\(.*\)/\(.*\),\2,')
-	tom=$(echo $to | sed -e 's,\(.*\)/\(.*\),\2,')
+	fromm=${from##*/}
+	tom=${to##*/}
 
 	if pick +$fromf important -list 2>&1 | egrep -q "^${fromm}\$"; then
 	    mark +$tof $tom -add -sequence important
